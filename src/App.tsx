6acec3005ee6224d5e4857e2da445886f0870f6d
Uzairshaikhh <uzair.shaikh.01kb@gmail.com>
2025-12-20 23:59:56 +0530

    Temporary: Replace StorePage with working placeholder - circular dependency detected

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Suspense } from "react";
-import { StorePage } from "./pages/Store";
 import { ToastProvider } from "./contexts/ToastContext";
 import { CartProvider } from "./contexts/CartContext";
 import { Favicon } from "./components/Favicon";
@@ -15,11 +13,21 @@ export default function App() {
             <Route
               path="/"
               element={
-                <Suspense fallback={<div style={{ padding: "20px", color: "#fff" }}>Loading store...</div>}>
-                  <StorePage />
-                </Suspense>
+                <div style={{ padding: "40px", color: "#fff", textAlign: "center" }}>
+                  <h1>🎌 AnimeDropZone Store</h1>
+                  <p>Store page is loading...</p>
+                  <p>If you see this, the app is working!</p>
+                  <p>We're rebuilding the store page to fix technical issues.</p>
+                  <p>Please check back soon!</p>
+                </div>
               }
             />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </ToastProvider>
+  );
+}
           </Routes>
         </Router>
       </CartProvider>
